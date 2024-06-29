fdc50e784d6835992927a9b60872917057cafc78
RomarioVieira123 <romario.lara.vieira@gmail.com>
2024-06-29 19:52:33 -0300

    Ajuste da porta da api

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from src.config.config import ENVIRON
 
 if __name__ == "__main__":
     if ENVIRON == 'dev':
-        uvicorn.run("src.app:app", host="127.0.0.1", port=8000, reload=True)
+        uvicorn.run("src.app:app", host="127.0.0.1", port=8001, reload=True)
     elif ENVIRON == 'prd' or ENVIRON == 'prod':
         uvicorn.run("src.app:app", host="0.0.0.0", port=80, reload=True)
